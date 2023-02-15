8289c88db3beabf58ce467562a52a25d835cbf33
Serp <serlordpasypkin@yandex.ru>
2023-02-15 20:46:15 +0300

    Добавил решение поставленной задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 ﻿
+string[] startArray = new string [] {"qwerty", "123321", "o", "hi"};
+
+int NewArraySize = 0;
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= 3)
+        NewArraySize++;        
+}
+
+string[] finalArray = new string[NewArraySize];
+
+int j = 0;
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= 3)
+    {
+        finalArray[j] = startArray[i];
+        j++;    
+    }
+}
+Console.Write("\n [{0}] ->",String.Join(", ", startArray));
+Console.WriteLine(" [{0}]",String.Join(", ", finalArray));
+
+
